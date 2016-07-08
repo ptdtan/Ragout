4b6ec104ab034c749a32d196da71e42d67d177e4
ptdtan <ptdtan@gmail.com>
2016-07-08 12:41:33 +0700

    add ref_genome

--- ragout/scaffolder/scaffolder.py
+++ ragout/scaffolder/scaffolder.py
@@ -91,7 +91,7 @@ def assign_scaffold_names(scaffolds, perm_container, ref_genome):
         assigned_names[scf] = name_str
         need_rev_compl[scf] = sign_agreement < total / 2
         if not need_rev_compl[scf] :
-            logger.debug("WARNING: Scaffold might have the reverse strand compare to the reference strand: %s",naming_ref )
+            logger.debug("WARNING: Scaffold might have the reverse strand compare to the reference strand: %s",ref_genome )
 
     #in case of same names
     same_names = defaultdict(list)
